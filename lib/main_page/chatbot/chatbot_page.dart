a92d2bdb9d38e28cb7d0c5514a09dd411dff0946
jeonghyeon_ohk <ohkgusuel@gmail.com>
2024-05-18 23:04:32 +0900

    feat : geolocator 패키지 연결 및 위치 정보 동의 구현

--- lib/main_page/chatbot/chatbot_page.dart
+++ lib/main_page/chatbot/chatbot_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class ChatbotPage extends StatelessWidget {
+class ChatbotPage extends StatefulWidget {
+  @override
+  _ChatbotPageState createState() => _ChatbotPageState();
+}
+
+class _ChatbotPageState extends State<ChatbotPage> {
+  OpenAIAssistant assistant = OpenAIAssistant();
+  String response = '';
+
+  @override
+  void initState() {
+    super.initState();
+    startChatbot();
+  }
+
+  void startChatbot() async {
+    try {
+      var assistantId = await assistant.createAssistant();
+      var threadId = await assistant.createThread();
+      await assistant.sendMessage(threadId, "I need to solve the equation `3x + 11 = 14`. Can you help me?");
+      var messages = await assistant.pollMessages(threadId);
+      setState(() {
+        response = messages[0]['content'];
+      });
+    } catch (e) {
+      setState(() {
+        response = 'Error: $e';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Chatbot 페이지'),
+      child: Text(response),
     );
   }
 }
--- lib/main_page/map/map_page.dart
+++ lib/main_page/map/map_page.dart
@@ -1,10 +1,42 @@
 import 'dart:async';
-import 'dart:developer'; // dart:developer 추가
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+  @override
+  initState() {
+    super.initState();
+
+    checkPermission();
+  }
+
+  checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      throw Exception('위치 기능을 활성화 해주세요.');
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+    }
+
+    if (checkedPermission != LocationPermission.always &&
+        checkedPermission != LocationPermission.whileInUse) {
+      throw Exception('위치 권한을 허가 해주세요.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +64,35 @@ class MapPage extends StatelessWidget {
       ),
       body: NaverMap(
         options: const NaverMapViewOptions(
+          initialCameraPosition: NCameraPosition(
+              target: NLatLng(
+                37.5214,
+                126.9246,
+              ),
+              zoom: 13,
+              bearing: 0,
+              tilt: 0),
           indoorEnable: true,
-          locationButtonEnable: false,
+          locationButtonEnable: true,
           consumeSymbolTapEvents: false,
         ),
-        onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
-          mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
+        onMapReady: (controller) async {
+          mapControllerCompleter.complete(controller);
           log("onMapReady", name: "onMapReady");
+
+          final marker = NMarker(
+              id: 'test',
+              position:
+              const NLatLng(37.506932467450326, 127.05578661133796));
+          final marker1 = NMarker(
+              id: 'test1',
+              position:
+              const NLatLng(37.606932467450326, 127.05578661133796));
+          controller.addOverlayAll({marker, marker1});
+
+          final onMarkerInfoWindow =
+          NInfoWindow.onMarker(id: marker.info.id, text: "건물");
+          marker.openInfoWindow(onMarkerInfoWindow);
         },
       ),
     );
