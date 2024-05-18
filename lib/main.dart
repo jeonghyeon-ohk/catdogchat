28b01c54bd75ea896eb62a9c6ced3a5c96cd6d67
jeonghyeon_ohk <ohkgusuel@gmail.com>
2024-05-18 18:40:53 +0900

    feat: main.dart에서 NaverMap 초기화 및 MapPage body에 지도 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
-import 'package:catdogchat/login/join_page.dart';
-import 'package:catdogchat/login/login_page.dart';
-import 'package:catdogchat/main_page/map/hospital_info_page.dart';
-import 'package:catdogchat/main_page/my/change_password.dart';
-import 'package:catdogchat/main_page/my/edit_profile.dart';
-import 'package:catdogchat/main_page/my/logout.dart';
-import 'package:catdogchat/main_page/my/withdraw.dart';
-import 'package:catdogchat/screen/splash_screen.dart';
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
+import 'login/join_page.dart';
+import 'login/login_page.dart';
 import 'main_page/home.dart';
+import 'main_page/map/hospital_info_page.dart';
+import 'main_page/my/change_password.dart';
+import 'main_page/my/edit_profile.dart';
+import 'main_page/my/logout.dart';
 import 'main_page/my/pet_edit_page.dart';
-void main() {
-  runApp(
-    MaterialApp(
+import 'main_page/my/withdraw.dart';
+import 'screen/splash_screen.dart';
+import 'main_page/map/map_page.dart';  // 새로 추가한 지도 페이지 import
+
+void main() async {
+  await _initialize();
+  runApp(MyApp());
+}
+
+// 지도 초기화하기
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+      clientId: 'ak8mch1b5s',     // 클라이언트 ID 설정
+      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -20,17 +42,17 @@ void main() {
       initialRoute: '/splash',
       routes: {
         '/splash': (BuildContext context) => SplashScreen(),
-        '/' : (BuildContext context) => MainPage(),
-        '/login' : (BuildContext context) => LoginPage(),
-        '/join' : (BuildContext context) => JoinPage(),
-        '/hospital' : (BuildContext context) => HospitalInfoPage(),
-        '/petEdit' : (BuildContext context) => PetEditPage(),
-        '/editProfile' : (BuildContext context) => EditProfile(),
-        '/password' : (BuildContext context) => ChangePassword(),
-        '/logout' : (BuildContext context) => Logout(),
-        '/withdraw' : (BuildContext context) => Withdraw(),
-
+        '/': (BuildContext context) => MainPage(),
+        '/login': (BuildContext context) => LoginPage(),
+        '/join': (BuildContext context) => JoinPage(),
+        '/hospital': (BuildContext context) => HospitalInfoPage(),
+        '/petEdit': (BuildContext context) => PetEditPage(),
+        '/editProfile': (BuildContext context) => EditProfile(),
+        '/password': (BuildContext context) => ChangePassword(),
+        '/logout': (BuildContext context) => Logout(),
+        '/withdraw': (BuildContext context) => Withdraw(),
+        '/map': (BuildContext context) => MapPage(),  // 새로 추가한 지도 페이지 route 설정
       },
-    ),
-  );
+    );
+  }
 }
--- lib/main_page/map/map_page.dart
+++ lib/main_page/map/map_page.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:developer'; // dart:developer 추가
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class MapPage extends StatelessWidget {
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +30,16 @@ class MapPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('지도 표시 영역'),
+      body: NaverMap(
+        options: const NaverMapViewOptions(
+          indoorEnable: true,
+          locationButtonEnable: false,
+          consumeSymbolTapEvents: false,
+        ),
+        onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
+          mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
+          log("onMapReady", name: "onMapReady");
+        },
       ),
     );
   }
