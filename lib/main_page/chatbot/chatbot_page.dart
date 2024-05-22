d03684d13c1a8905dc2d087459e485a791923905
jeonghyeon_ohk <ohkgusuel@gmail.com>
2024-05-22 21:27:37 +0900

    feat : 사진 첨부 기능 및 AI 서버 이미지 전송 및 응답 처리 연결

--- lib/main_page/chatbot/chatbot_page.dart
+++ lib/main_page/chatbot/chatbot_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   runApp(MaterialApp(
@@ -18,10 +21,12 @@ class ChatbotPage extends StatefulWidget {
 }
 
 class _ChatbotPageState extends State<ChatbotPage> {
-  final String apiKey = "sk-proj-9RJWJitV6GK8ihWJKmDRT3BlbkFJ7arCRY4px1KaKXn95w8n";
+  final String apiKey =
+      "sk-proj-9RJWJitV6GK8ihWJKmDRT3BlbkFJ7arCRY4px1KaKXn95w8n";
   final String model = "gpt-4";
   final TextEditingController _messageController = TextEditingController();
   final List<Map<String, String?>> _messages = [];
+  Uint8List? _selectedImage;
 
   @override
   void initState() {
@@ -35,11 +40,197 @@ class _ChatbotPageState extends State<ChatbotPage> {
         "role": "assistant",
         "content": "안녕하세요! 반려동물에 관한 질문이 있으신가요? 도와드릴 수 있어요"
       });
+      _messages.add({
+        "role": "assistant",
+        "content": "또는 반려동물의 피부 혹은 안구 사진을 첨부해주시면 어떤 질환인지 판단해드릴게요"
+      });
     });
   }
 
+  final String instruction = """
+This GPT, the Pet Health Assistant, is specifically designed for Korean-speaking users. It provides general insights into pets' health based on symptoms or behaviors described in Korean. If a user does not specify the type of pet, the GPT will ask the user to specify the animal type before proceeding with advice. In non-emergency, everyday situations, this GPT will incorporate light humor in its responses to keep the interaction engaging. It offers potential causes and solutions in Korean before suggesting a consultation with a professional for definitive advice. It is essential for the GPT to guide users towards professional help without giving definite diagnoses, ensuring all interactions are in Korean. Additionally, when users ask questions with unclear subjects such as 'What should I eat tonight?' or 'Which exercise would be good?', the GPT will assume the subject is the user's pet and respond accordingly.
+""";
+
   Future<void> sendMessage(String content) async {
-    // API 호출 및 로직 구현
+    setState(() {
+      _messages.add({
+        "role": "user",
+        "content": content,
+      });
+    });
+
+    try {
+      var response = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $apiKey',
+        },
+        body: utf8.encode(jsonEncode({
+          "model": model,
+          "messages": [
+            {"role": "system", "content": instruction},
+            ..._messages
+                .map((message) =>
+                    {"role": message['role'], "content": message['content']})
+                .toList(),
+          ],
+          "max_tokens": 1000,
+          "temperature": 0.5,
+        })),
+      );
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(utf8.decode(response.bodyBytes));
+        setState(() {
+          _messages.add({
+            "role": "assistant",
+            "content": data['choices'][0]['message']['content'].trim(),
+          });
+        });
+      } else {
+        setState(() {
+          _messages.add({
+            "role": "assistant",
+            "content": "Error: ${response.reasonPhrase}",
+          });
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _messages.add({"role": "assistant", "content": "Error: $e"});
+      });
+    }
+  }
+
+  Future<void> sendImageToAiServer(Uint8List image) async {
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse(
+            'http://13.48.15.160:5000/predict'), // Replace with your AI server's URL
+      );
+
+      // Convert Uint8List to MultipartFile
+      request.files.add(http.MultipartFile.fromBytes(
+        'file',
+        image,
+        filename: 'upload.jpg', // You can use any filename you prefer
+      ));
+
+      var streamedResponse = await request.send();
+      var response = await http.Response.fromStream(streamedResponse);
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+
+        // Ensure the 'prediction' key exists in the response
+        if (data.containsKey('prediction')) {
+          String aiServerResponse = data['prediction'];
+          // Send AI server response to GPT-4
+          await handleServerResponse(aiServerResponse);
+        } else {
+          setState(() {
+            _messages.add({
+              "role": "assistant",
+              "content":
+                  "Error: The key 'prediction' was not found in the server response.",
+            });
+          });
+        }
+      } else {
+        setState(() {
+          _messages.add({
+            "role": "assistant",
+            "content":
+                "Error: Server responded with status code ${response.statusCode}",
+          });
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _messages.add({"role": "assistant", "content": "Error: $e"});
+      });
+    }
+  }
+
+  Future<void> handleServerResponse(String aiServerResponse) async {
+    setState(() {
+      _messages.add({"role": "user", "content": aiServerResponse});
+    });
+
+    try {
+      var response = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $apiKey',
+        },
+        body: utf8.encode(jsonEncode({
+          'model': model,
+          'messages': [
+            {
+              'role': 'system',
+              'content': instruction,
+            },
+            ..._messages
+                .map((message) => {
+                      'role': message['role'],
+                      'content': message['content'],
+                    })
+                .toList(),
+          ],
+          'max_tokens': 1000,
+          'temperature': 0.5,
+        })),
+      );
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(utf8.decode(response.bodyBytes));
+        setState(() {
+          _messages.add({
+            "role": "assistant",
+            "content": data['choices'][0]['message']['content'].trim(),
+          });
+        });
+      } else {
+        var errorData = jsonDecode(utf8.decode(response.bodyBytes));
+        setState(() {
+          _messages.add({
+            "role": "assistant",
+            "content":
+                "Error: ${response.reasonPhrase}\n${errorData['error']['message']}",
+          });
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _messages.add({"role": "assistant", "content": "Error: $e"});
+      });
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      final bytes = await pickedFile.readAsBytes();
+      setState(() {
+        _selectedImage = bytes;
+      });
+    }
+  }
+
+  Widget _buildImagePreview() {
+    return _selectedImage == null
+        ? Container()
+        : SizedBox(
+            height: 100,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.memory(_selectedImage!), // Use Image.memory
+            ),
+          );
   }
 
   @override
@@ -70,18 +261,27 @@ class _ChatbotPageState extends State<ChatbotPage> {
                 var message = _messages[index];
                 bool isUser = message['role'] == 'user';
                 return ListTile(
-                  leading: isUser ? null : SizedBox(width: screenWidth * 0.1, child: Image.asset('asset/img/logo.png')),
+                  leading: isUser
+                      ? null
+                      : SizedBox(
+                          width: screenWidth * 0.1,
+                          child: Image.asset('asset/img/logo.png')),
                   title: Align(
-                    alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
+                    alignment:
+                        isUser ? Alignment.centerRight : Alignment.centerLeft,
                     child: Container(
-                      padding: EdgeInsets.symmetric(vertical: screenWidth * 0.02, horizontal: screenWidth * 0.04),
+                      padding: EdgeInsets.symmetric(
+                          vertical: screenWidth * 0.02,
+                          horizontal: screenWidth * 0.04),
                       decoration: BoxDecoration(
                         color: isUser ? Colors.blue[300] : Colors.grey[200],
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: Text(
                         message['content'] ?? '',
-                        style: TextStyle(color: isUser ? Colors.white : Colors.black, fontSize: screenWidth * 0.035),
+                        style: TextStyle(
+                            color: isUser ? Colors.white : Colors.black,
+                            fontSize: screenWidth * 0.035),
                       ),
                     ),
                   ),
@@ -89,6 +289,7 @@ class _ChatbotPageState extends State<ChatbotPage> {
               },
             ),
           ),
+          _buildImagePreview(),
           Padding(
             padding: EdgeInsets.all(screenWidth * 0.02),
             child: Row(
@@ -98,54 +299,39 @@ class _ChatbotPageState extends State<ChatbotPage> {
                     controller: _messageController,
                     decoration: InputDecoration(
                       hintText: '반려동물의 상태를 문의해 주세요!',
-                      hintStyle: TextStyle(fontSize: screenWidth * 0.035), // 조절된 힌트 글씨 크기
+                      hintStyle: TextStyle(
+                          fontSize: screenWidth * 0.035), // 조절된 힌트 글씨 크기
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide.none,
                       ),
                       fillColor: Colors.grey[200],
                       filled: true,
-                      suffixIcon: IconButton(icon: Icon(Icons.camera_alt), onPressed: () {}),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.photo),
+                        onPressed: _pickImage,
+                      ),
                     ),
                   ),
                 ),
                 IconButton(
                   icon: Icon(Icons.send, size: screenWidth * 0.07),
-                  onPressed: () {
-                    // 메시지 전송 로직
+                  onPressed: () async {
+                    final content = _messageController.text;
+                    if (_selectedImage != null) {
+                      await sendImageToAiServer(_selectedImage!);
+                      setState(() {
+                        _selectedImage = null;
+                      });
+                    }
+                    if (content.isNotEmpty) {
+                      await sendMessage(content);
+                      _messageController.clear();
+                    }
                   },
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CategoryPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('카테고리를 선택하세요')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ListTile(
-            title: Text('안구'),
-            leading: Icon(Icons.visibility),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('피부'),
-            leading: Icon(Icons.texture),
-            onTap: () {
-              Navigator.pop(context);
-            },
           ),
         ],
       ),
